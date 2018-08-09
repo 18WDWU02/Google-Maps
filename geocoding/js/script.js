169de61f36b76fc94f35bfd83b3ac50f4c125933
18wdwu07 <40220616+18wdwu07@users.noreply.github.com>
2018-08-09 18:10:19 +1200

    displays latitude longitude of  a place

---
+++ geocoding/js/script.js
@@ -0,0 +1,26 @@
+
+function showResult(result) {
+    document.getElementById('latitude').value = result.geometry.location.lat();
+    document.getElementById('longitude').value = result.geometry.location.lng();
+}
+
+function getLatitudeLongitude(callback, address) {
+    // If adress is not supplied, use default value 'Ferrol, Galicia, Spain'
+    address = address || 'Ferrol, Galicia, Spain';
+    // Initialize the Geocoder
+    geocoder = new google.maps.Geocoder();
+    if (geocoder) {
+        geocoder.geocode({
+            'address': address
+        }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                callback(results[0]);
+            }
+        });
+    }
+}
+function inputAddress(){
+    var address = document.getElementById('address').value;
+    console.log(address);
+    getLatitudeLongitude(showResult, address)
+}
